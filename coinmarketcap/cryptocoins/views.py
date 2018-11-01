cf95d9349042b34312692cedcb31764c1f94f644
Martin Zugnoni <martin.zugnoni@gmail.com>
2018-11-01 20:07:34 -0300

    Add filters to list view

--- coinmarketcap/cryptocoins/views.py
+++ coinmarketcap/cryptocoins/views.py
@@ -4,6 +4,23 @@ from cryptocoins.models import Cryptocurrency
 
 def index(request):
     coins = Cryptocurrency.objects.all().order_by('rank')
+
+    if 'order_by' in request.GET:
+        field = request.GET['order_by']
+        if field in dir(Cryptocurrency):
+            order_type = request.GET.get('order_type', 'asc')
+            if order_type == 'desc':
+                field = '-{}'.format(field)
+            coins = coins.order_by(field)
+
+    if 'min_price' in request.GET:
+        try:
+            min_value = int(request.GET['min_price'])
+        except ValueError:
+            pass
+        else:
+            coins = coins.filter(price_usd__gte=min_value)
+
     return render(request, 'index.html', {
         'coins': coins
     })
