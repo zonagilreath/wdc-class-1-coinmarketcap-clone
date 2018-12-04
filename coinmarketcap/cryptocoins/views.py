d2ebfca8dc3f7cdf8846bdfff215876950625e5b
Santiago Basulto <santiago.basulto@gmail.com>
2018-12-04 18:11:32 -0300

    Step 4 working

--- coinmarketcap/cryptocoins/views.py
+++ coinmarketcap/cryptocoins/views.py
@@ -3,8 +3,20 @@ from .models import Cryptocurrency
 
 
 def index(request):
-    coins = Cryptocurrency.objects.all().order_by('rank')
+    order_param = request.GET.get('order_param', 'rank')
+    order_direction = request.GET.get('order_direction', 'asc')
+    order_by = 'rank'
+
+    coins = Cryptocurrency.objects.all()
+    if order_param == 'price':
+        order_by = 'price_usd'
+    if order_direction == 'desc':
+        order_by = '-' + order_by
+
+    coins = coins.order_by(order_by)
     return render(request, 'index.html', {
+        'order_param': order_param,
+        'order_direction': order_direction,
         'coins': coins
     })
 
